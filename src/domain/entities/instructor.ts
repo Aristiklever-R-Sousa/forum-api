cb370a07d000b06fbb2e96fd118558b57feef3a8
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2025-06-22 18:31:18 -0300

    :recycle: refactor: add some atributes to entities

--- src/domain/entities/instructor.ts
+++ src/domain/entities/instructor.ts
@@ -2,6 +2,9 @@ import { Entity } from "../../core/entities/entity"
 
 interface InstructorInterface {
     name: string
+
+    createdAt: Date
+    updatedAt?: Date
 }
 
 export class Instructor extends Entity<InstructorInterface> {
--- src/domain/entities/student.ts
+++ src/domain/entities/student.ts
@@ -2,6 +2,9 @@ import { Entity } from "../../core/entities/entity"
 
 interface StudentInterface {
     name: string
+
+    createdAt: Date
+    updatedAt?: Date
 }
 
 export class Student extends Entity<StudentInterface> {
