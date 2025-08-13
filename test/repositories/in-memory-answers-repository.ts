344e33c84bbd7b8ee83c9b2f41bb21a16930361d
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2025-08-12 21:48:05 -0300

    feat: add fetch question answers

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,4 +1,5 @@
 /* eslint-disable prettier/prettier */
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
@@ -11,6 +12,14 @@ export class InMemoryAnswersRepository implements AnswersRepository {
         if (!answer) return null
 
         return answer
+    }
+
+    async findManyByQuestionId(questionId: string, { page }: PaginationParams): Promise<Answer[]> {
+        const answers = this.items
+            .filter(item => item.questionId.toString() === questionId)
+            .slice((page - 1) * 20, page * 20)
+
+        return answers
     }
 
     async create(answer: Answer): Promise<void> {
