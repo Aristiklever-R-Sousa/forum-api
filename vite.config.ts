9da2354abe89e0db1c2d000d942d87843ab51e82
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2025-09-28 18:28:41 -0300

    feat: add watched list class

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-    plugins: [tsConfigPaths()],
+  plugins: [tsConfigPaths()],
 
-    test: {
-        globals: true
-    }
+  test: {
+    globals: true,
+  },
 })
