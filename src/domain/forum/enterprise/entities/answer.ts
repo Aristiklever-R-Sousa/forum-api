925b4a2513bc58334e2f7e83515cd44b263a6ca4
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2025-09-29 23:59:00 -0300

    feat: add answer created event

--- src/domain/forum/enterprise/entities/answer.ts
+++ src/domain/forum/enterprise/entities/answer.ts
@@ -1,7 +1,8 @@
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 import { AnswerAttachmentList } from './answer-attachment-list'
+import { AggregateRoot } from '@/core/entities/aggregate-root'
+import { AnswerCreatedEvent } from '../events/answer-created-event'
 
 export interface AnswerProps {
   authorId: UniqueEntityId
@@ -13,23 +14,7 @@ export interface AnswerProps {
   updatedAt?: Date
 }
 
-export class Answer extends Entity<AnswerProps> {
-  static create(
-    props: Optional<AnswerProps, 'createdAt' | 'attachments'>,
-    id?: UniqueEntityId,
-  ) {
-    const answer = new Answer(
-      {
-        ...props,
-        attachments: props.attachments ?? new AnswerAttachmentList(),
-        createdAt: new Date(),
-      },
-      id,
-    )
-
-    return answer
-  }
-
+export class Answer extends AggregateRoot<AnswerProps> {
   get authorId() {
     return this.props.authorId
   }
@@ -70,5 +55,27 @@ export class Answer extends Entity<AnswerProps> {
   set attachments(attachments: AnswerAttachmentList) {
     this.props.attachments = attachments
     this.touch()
+  }
+
+  static create(
+    props: Optional<AnswerProps, 'createdAt' | 'attachments'>,
+    id?: UniqueEntityId,
+  ) {
+    const answer = new Answer(
+      {
+        ...props,
+        attachments: props.attachments ?? new AnswerAttachmentList(),
+        createdAt: new Date(),
+      },
+      id,
+    )
+
+    const isNewAnswer = !id
+
+    if (isNewAnswer) {
+      answer.addDomainEvent(new AnswerCreatedEvent(answer))
+    }
+
+    return answer
   }
 }
